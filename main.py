dc2209096c64a97f35f6f819dd6e1459ceac171f
Tristenx <1604tristenarron@gmail.com>
2025-05-31 14:49:04 +0100

    check if user input is number

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 def addition(num1, num2):
-    result = float(num1) + float(num2)
+    result = num1 + num2
     return result
 
 
 def subtraction(num1, num2):
-    result = float(num1) - float(num2)
+    result = num1 - num2
     return result
 
 
 def multiplication(num1, num2):
-    result = float(num1) * float(num2)
+    result = num1 * num2
     return result
 
 
 def division(num1, num2):
-    result = float(num1) / float(num2)
+    result = num1 / num2
     return result
 
 
+def check_if_num(input):
+    try:
+        num = float(input)
+        return True
+    except ValueError:
+        return False
+
+
 num1 = input("Enter a number: ")
+while not check_if_num(num1):
+    print("Invalid input")
+    num1 = input("Enter a number: ")
+num1 = float(num1)
 num2 = input("Enter a second number: ")
+while not check_if_num(num2):
+    print("Invalid input")
+    num2 = input("Enter a second number: ")
+num2 = float(num2)
 operation = input("Enter an operation (+,-,x,/): ")
 
 if operation == "+":
@@ -31,4 +47,4 @@ elif operation == "x":
 elif operation == "/":
     print(division(num1, num2))
 else:
-    print("Invalid input")
+    print("Invalid operation")
