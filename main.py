41862eb493f2e01163feb809761c0a2d0f0102cc
Tristenx <1604tristenarron@gmail.com>
2025-05-31 12:56:08 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+def addition(num1, num2):
+    pass
+
+
+def subtraction(num1, num2):
+    pass
+
+
+def multiplication(num1, num2):
+    pass
+
+
+def division(num1, num2):
+    pass
+
+
+num1 = input("Enter a number: ")
+num2 = input("Enter a second number: ")
+operation = input("Enter an operation (+,-,x,/): ")
