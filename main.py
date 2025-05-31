4ab332a0b0bb106e04df73f90d72876559fe23e8
Tristenx <1604tristenarron@gmail.com>
2025-05-31 13:06:14 +0100

    read operation

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ def division(num1, num2):
 num1 = input("Enter a number: ")
 num2 = input("Enter a second number: ")
 operation = input("Enter an operation (+,-,x,/): ")
+
+if operation == "+":
+    print(addition(num1, num2))
+elif operation == "-":
+    print(subtraction(num1, num2))
+elif operation == "x":
+    print(multiplication(num1, num2))
+elif operation == "/":
+    print(division(num1, num2))
+else:
+    print("Invalid input")
