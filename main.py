93a4d4d2ea4d575decef2f26b73646e23cc669bb
Tristenx <1604tristenarron@gmail.com>
2025-06-01 20:29:02 +0100

    type in display

--- main.py
+++ main.py
@@ -54,6 +54,91 @@ while calc_on:
 
 print(current_num)
 
+# ----------------------------------------------------------------------------------------------------------------#
+
+current_ans = 0
+display_num = ""
+
+
+def zero_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "0"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
+
+def one_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "1"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
+
+def two_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "2"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
+
+def three_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "3"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
+
+def four_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "4"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
+
+def five_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "5"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
+
+def six_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "6"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
+
+def seven_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "7"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
+
+def eight_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "8"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
+
+def nine_func():
+    global display_num
+    display_num = str(display_num)
+    display_num += "9"
+    display_num = int(display_num)
+    canvas.itemconfig(display, text=display_num)
+
 
 window = Tk()
 window.title("Calculator")
@@ -61,19 +146,19 @@ window.config(padx=20, pady=20)
 
 canvas = Canvas(width=200, height=100)
 display = canvas.create_text(
-    100, 50, text="this is a display", justify="center", width=200, font=("San Fransisco", 20))
+    100, 50, text=display_num, justify="center", width=200, font=("San Fransisco", 20))
 canvas.grid(row=0, column=0, columnspan=4)
 
 seven_button = Button(text=7, font=("San Fransisco", 20),
-                      relief="groove", width=3, height=1)
+                      relief="groove", width=3, height=1, command=seven_func)
 seven_button.grid(row=1, column=0)
 
 eight_button = Button(text=8, font=("San Fransisco", 20),
-                      relief="groove", width=3, height=1)
+                      relief="groove", width=3, height=1, command=eight_func)
 eight_button.grid(row=1, column=1)
 
 nine_button = Button(text=9, font=("San Fransisco", 20),
-                     relief="groove", width=3, height=1)
+                     relief="groove", width=3, height=1, command=nine_func)
 nine_button.grid(row=1, column=2)
 
 divide_button = Button(text="÷", font=("San Fransisco", 20),
@@ -81,15 +166,15 @@ divide_button = Button(text="÷", font=("San Fransisco", 20),
 divide_button.grid(row=1, column=3)
 
 four_button = Button(text=4, font=("San Fransisco", 20),
-                     relief="groove", width=3, height=1)
+                     relief="groove", width=3, height=1, command=four_func)
 four_button.grid(row=2, column=0)
 
 five_button = Button(text=5, font=("San Fransisco", 20),
-                     relief="groove", width=3, height=1)
+                     relief="groove", width=3, height=1, command=five_func)
 five_button.grid(row=2, column=1)
 
 six_button = Button(text=6, font=("San Fransisco", 20),
-                    relief="groove", width=3, height=1)
+                    relief="groove", width=3, height=1, command=six_func)
 six_button.grid(row=2, column=2)
 
 times_button = Button(text="x", font=("San Fransisco", 20),
@@ -97,15 +182,15 @@ times_button = Button(text="x", font=("San Fransisco", 20),
 times_button.grid(row=2, column=3)
 
 one_button = Button(text=1, font=("San Fransisco", 20),
-                    relief="groove", width=3, height=1)
+                    relief="groove", width=3, height=1, command=one_func)
 one_button.grid(row=3, column=0)
 
 two_button = Button(text=2, font=("San Fransisco", 20),
-                    relief="groove", width=3, height=1)
+                    relief="groove", width=3, height=1, command=two_func)
 two_button.grid(row=3, column=1)
 
 three_button = Button(text=3, font=("San Fransisco", 20),
-                      relief="groove", width=3, height=1)
+                      relief="groove", width=3, height=1, command=three_func)
 three_button.grid(row=3, column=2)
 
 minus_button = Button(text="-", font=("San Fransisco", 20),
@@ -113,7 +198,7 @@ minus_button = Button(text="-", font=("San Fransisco", 20),
 minus_button.grid(row=3, column=3)
 
 zero_button = Button(text=0, font=("San Fransisco", 20),
-                     relief="groove", width=3, height=1)
+                     relief="groove", width=3, height=1, command=zero_func)
 zero_button.grid(row=4, column=0)
 
 decimal_button = Button(text=".", font=("San Fransisco", 20),
