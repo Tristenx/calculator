bd2ebf796c0ecd9bbd60e241e64f1d9a986dc4f6
Tristenx <1604tristenarron@gmail.com>
2025-06-01 02:15:38 +0100

    created the gui for the calc

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from tkinter import *
+
+
 def addition(num1, num2):
     return num1 + num2
 
@@ -50,3 +53,79 @@ while calc_on:
         print("Invalid operation")
 
 print(current_num)
+
+
+window = Tk()
+window.title("Calculator")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=100)
+display = canvas.create_text(
+    100, 50, text="this is a display", justify="center", width=200, font=("San Fransisco", 20))
+canvas.grid(row=0, column=0, columnspan=4)
+
+seven_button = Button(text=7, font=("San Fransisco", 20),
+                      relief="groove", width=3, height=1)
+seven_button.grid(row=1, column=0)
+
+eight_button = Button(text=8, font=("San Fransisco", 20),
+                      relief="groove", width=3, height=1)
+eight_button.grid(row=1, column=1)
+
+nine_button = Button(text=9, font=("San Fransisco", 20),
+                     relief="groove", width=3, height=1)
+nine_button.grid(row=1, column=2)
+
+divide_button = Button(text="÷", font=("San Fransisco", 20),
+                       relief="groove", width=3, height=1)
+divide_button.grid(row=1, column=3)
+
+four_button = Button(text=4, font=("San Fransisco", 20),
+                     relief="groove", width=3, height=1)
+four_button.grid(row=2, column=0)
+
+five_button = Button(text=5, font=("San Fransisco", 20),
+                     relief="groove", width=3, height=1)
+five_button.grid(row=2, column=1)
+
+six_button = Button(text=6, font=("San Fransisco", 20),
+                    relief="groove", width=3, height=1)
+six_button.grid(row=2, column=2)
+
+times_button = Button(text="x", font=("San Fransisco", 20),
+                      relief="groove", width=3, height=1)
+times_button.grid(row=2, column=3)
+
+one_button = Button(text=1, font=("San Fransisco", 20),
+                    relief="groove", width=3, height=1)
+one_button.grid(row=3, column=0)
+
+two_button = Button(text=2, font=("San Fransisco", 20),
+                    relief="groove", width=3, height=1)
+two_button.grid(row=3, column=1)
+
+three_button = Button(text=3, font=("San Fransisco", 20),
+                      relief="groove", width=3, height=1)
+three_button.grid(row=3, column=2)
+
+minus_button = Button(text="-", font=("San Fransisco", 20),
+                      relief="groove", width=3, height=1)
+minus_button.grid(row=3, column=3)
+
+zero_button = Button(text=0, font=("San Fransisco", 20),
+                     relief="groove", width=3, height=1)
+zero_button.grid(row=4, column=0)
+
+decimal_button = Button(text=".", font=("San Fransisco", 20),
+                        relief="groove", width=3, height=1)
+decimal_button.grid(row=4, column=1)
+
+equals_button = Button(text="=", font=("San Fransisco", 20),
+                       relief="groove", width=3, height=1)
+equals_button.grid(row=4, column=2)
+
+plus_button = Button(text="+", font=("San Fransisco", 20),
+                     relief="groove", width=3, height=1)
+plus_button.grid(row=4, column=3)
+
+window.mainloop()
